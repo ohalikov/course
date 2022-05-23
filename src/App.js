02cef89386797882717b16968fe5b79b54eb32f3
Oleg Khalikov <olegway31@gmail.com>
2022-05-23 23:33:45 +0500

    first

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
-import React from "react";
-import './css/App.css';
-import MovebleBlock from './components/MovebleBlock';
+import React, {useState} from "react";
+import PostItem from "./components/PostItem";
+
+import './styles/App.css';
+
 
 function App() {
+  const [posts, setPosts] = useState([
+    {id: 1, title: 'JavaScript', body: 'Description 1'},
+    {id: 2, title: 'Python', body: 'Description 2 '},
+    {id: 3, title: 'Django', body: 'Descriptio 3 n'},
+    {id: 4, title: 'GIT', body: 'Description 4 '},
+    {id: 5, title: 'Linux', body: 'Description 5'}
+  ])
 
   return (
     <div className="App">
-      <MovebleBlock id={1} />
+      <h1 style={{textAlign: 'center'}}>Список Постов</h1>
+      {posts.map(post => 
+        <PostItem post={post} key={post.id}/>
+      )}
     </div>
   );
 }
